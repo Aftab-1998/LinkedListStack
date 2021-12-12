63e28b7826ca6416ec8a5582920e75966ae2aa88
Aftab-1998 <alam41976@gmail.com>
2021-12-12 22:17:24 +0530

    [Aftab3] Add ; Added the queue list

--- LinkedListStack/Program.cs
+++ LinkedListStack/Program.cs
@@ -5,14 +5,21 @@ class program
 {
     static void Main(string[] args)
     {
-        LinkedListStack linkedListStack = new LinkedListStack();
-        linkedListStack.Push(70);
-        linkedListStack.Push(30);
-        linkedListStack.Push(56);
-        linkedListStack.Display();
-        linkedListStack.peek();
-        linkedListStack.pop();
-        linkedListStack.Display();
+        //LinkedListStack linkedListStack = new LinkedListStack();
+        //linkedListStack.Push(70);
+        //linkedListStack.Push(30);
+        //linkedListStack.Push(56);
+        //linkedListStack.Display();
+        //linkedListStack.peek();
+        //linkedListStack.pop();
+        //linkedListStack.isEmpty();
+       //linkedListStack.Display();
+
+        LinkedListQueue linkedListQueue = new LinkedListQueue();
+        linkedListQueue.Enqueue(56);
+        linkedListQueue.Enqueue(30);
+        linkedListQueue.Enqueue(70);
+        linkedListQueue.Display();
 
     }
 }
