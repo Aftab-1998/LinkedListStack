93408474d8e3cac15eba03cf529a9827b344063c
Aftab-1998 <alam41976@gmail.com>
2021-12-12 19:51:23 +0530

    [Aftab1] Add ; Added the list of stacks

---
+++ LinkedListStack/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using DataStructureStacks;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        LinkedListStack linkedListStack = new LinkedListStack();
+        linkedListStack.Push(70);
+        linkedListStack.Push(30);
+        linkedListStack.Push(56);
+        linkedListStack.Display();
+
+    }
+}
